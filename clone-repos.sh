4a344c03c09e13fdb6bb351f8ca25090dc62671f
Vinicius Guimarães <vg.guimaraes@hotmail.com>
2024-01-28 13:53:54 -0300

    add postman collection

--- clone-repos.sh
+++ clone-repos.sh
@@ -3,3 +3,5 @@
 git clone git@github.com:VGuimaraes5/fiap-restaurant-pagamento-ms.git Pagamento
 git clone git@github.com:VGuimaraes5/fiap-restaurant-pedido-ms.git Pedido
 git clone git@github.com:VGuimaraes5/fiap-restaurant-producao-ms.git Producao
+git clone git@github.com:VGuimaraes5/fiap-restaurant-terraform-ms.git Infrastructure-terraform
+git clone git@github.com:VGuimaraes5/fiap-auth-lambda.git fiap-auth-lambda
